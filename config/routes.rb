3343bed182cef57176ed0472f71c7ac5472f4a17
Shinya Yamasaki <152153760+yksy97@users.noreply.github.com>
2023-12-04 03:27:15 +0000

    第1章完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  get 'homes/top'
+  get '/new' => 'lists#new'
+  get '/show' => 'lists#show'
+  get '/edit' => 'lists#edit'
+  get '/index' => 'lists#index' 
+  get '/top' => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
